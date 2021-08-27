9d7ce09bbdc0395125796a4f500eb9b594a25f2c
subaraj <89238320+sbrj4901@users.noreply.github.com>
2021-08-27 20:03:25 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+DATA TYPES
+a=10 int
+s="abc" str
+M=20.4 float
+L=[10.20'30'] list
+d={"10":10,"b":20} dict
+c={10,20,20,10,11} set
+ set is non repeated values
+ unorder sequence
+ dublicate not allow
+Tuble:(1,2,3)
+boolean:a=True
+        b=False
+int
+N=100
+ operators +,-,/,*
+equal==b
+not equal to
+a!=b
