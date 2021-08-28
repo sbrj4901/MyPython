e90d4d24a09190c4e0012c65fd3ba907e5bcb6bc
subaraj <89238320+sbrj4901@users.noreply.github.com>
2021-08-28 14:39:40 +0530

    Add files via upload

---
+++ Listdata.py
@@ -0,0 +1,28 @@
+# LIST DATA TYPES
+L=['hello','world','love','singing','world',2]
+print(dir(L))
+print(L.index(2))
+print('index is',L.index(2))
+print('count is',L.count('world'))
+L.pop()
+print(L)
+L1=['python','program']
+L.extend(L1)
+print(L)
+L.remove('love')
+print(L)
+L.reverse()
+print(L)
+L3=[1,9,7,5,8,6]
+L3.sort()
+print(L3)
+L3.append('j')
+print(L3)
+L3.clear()
+print(L3)
+
+
+# l=['hello''world''love''singinig']
+# idx=l.index('love')
+# l.pop(idx l.insert(idx,'like')
+# print (dir(l))
