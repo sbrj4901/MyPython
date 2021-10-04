e3a97f5b6245eb3f0b156062acdd8dfd62de3093
subaraj <89238320+sbrj4901@users.noreply.github.com>
2021-10-04 22:30:59 +0530

    Add files via upload

--- pract3.py
+++ pract3.py
@@ -1,6 +1,20 @@
-# use break
+# use break after print statement
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
   if x == "banana":
     break
+
+# use break before print statement
+fruits = ["apple",'banana','cherry']
+for x in fruits:
+	if x == 'banana':
+	 break
+	print(x)
+
+# use continue
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+     continue
+    print(x)
