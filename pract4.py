451f2beb71916bcd55bd0f67e48c4793fcba5f07
subaraj <89238320+sbrj4901@users.noreply.github.com>
2021-10-06 22:48:24 +0530

    Add files via upload

--- pract4.py
+++ pract4.py
@@ -5,3 +5,13 @@ if b > a:
   print("b is greater than a")
 elif a == b:
   print("a and b are equal")
+
+# Else condition
+a = 255
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
