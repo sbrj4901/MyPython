c029a98b3be0a1ffa4e2b9b15a984cb58947f92e
subaraj <89238320+sbrj4901@users.noreply.github.com>
2021-10-17 22:53:54 +0530

    Add files via upload

---
+++ KeyWord_arg.py
@@ -0,0 +1,8 @@
+# Python program to demonstrate Keyword Arguments
+def student(firstname, lastname):
+    print(firstname, lastname)
+ 
+ 
+# Keyword arguments
+student(firstname='ram', lastname='Ganesh')
+student(lastname='Sathish', firstname='Kumar')
